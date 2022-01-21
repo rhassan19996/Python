4a34ba6ad38a4f5dd657f3ed00bdb26462fe19b9
Ryan <rhassan19996@gmail.com>
2022-01-20 22:55:30 -0500

    Swap values in lists and shortcut

---
+++ PythonTutorial/BFS_TraversalLeetCode102.py
@@ -0,0 +1,7 @@
+# Link: https://leetcode.com/problems/binary-tree-level-order-traversal/
+# Given the root of a binary tree, return the level order traversal of its nodes' values.
+# (i.e., from left to right, level by level).
+
+# Input: root = [3,9,20,null,null,15,7]
+# Output: [[3],[9,20],[15,7]]
+# For BFS Breadth first search use Queue to implement the array
--- PythonTutorial/forloop.py
+++ PythonTutorial/forloop.py
@@ -1,5 +1,33 @@
-a= ['banana', 'apple', 'microsoft']
+a = ['banana', 'apple', 'microsoft']
 
 for items in a:
     print(a)
 
+b = [20, 10, 5]
+total = 0
+for i in b:
+    total += i
+print(total)
+
+print(list(range(1, 8)))
+
+total3 = 0
+for i in range(1, 8):
+    if i % 3 == 0:
+        total3 += i
+print(total3)
+
+total4 = 0
+for i in range(1, 100):
+    if i % 3 == 0 or i % 5 == 0:
+        total4 += i
+print(total4)
+
+total5 = 0
+given_list = [7, 5, 4, 4, 3, 1, -2, -3, -5, -7]
+j = len(given_list) - 1
+
+while given_list[j] < 0:
+    total5 += given_list[j]
+    j -= 1
+print(total5)
