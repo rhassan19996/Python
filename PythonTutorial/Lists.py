234b41379ddc79ba18e6e34b15edb18c5a2baaec
Ryan <rhassan19996@gmail.com>
2022-01-20 22:01:34 -0500

    Swap values in lists and shortcut

--- PythonTutorial/Lists.py
+++ PythonTutorial/Lists.py
@@ -1,20 +1,38 @@
 a = [3, 10, -1]
-
 print(a)
-
+# [3, 10, -1]
 a.append(1)
-
 print(a)
-
+# [3, 10, -1, 1]
 a.append("hello")
 print(a)
-
+# [3, 10, -1, 1, 'hello']
 a.append([1, 2])
 print(a)
-
+# [3, 10, -1, 1, 'hello', [1, 2]]
 a.pop()
 a.pop()
 print(a)
-
+# [3, 10, -1, 1]
 print(a[0])
+# 3
 print(a[3])
+# 1
+
+# assign a new number
+a[0] = 100
+print(a)
+
+b = ['banana', 'apple', 'Microsoft']
+print("Before swap: ")
+print(b)
+#temp = b[0]
+#b[0] = b[1]
+#b[1] = temp
+#print("After swap")
+#print(b)
+#shortcut for swap
+b[0], b[1] = b[1], b[0]
+print(b)
+
+
