d75186cfdbea90f307845258ff1eee77c1e07550
Ryan <rhassan19996@gmail.com>
2022-01-23 23:58:28 -0500

    added if statement to check last level and add that to the list

--- PythonTutorial/BFS_TraversalLeetCode102.py
+++ PythonTutorial/BFS_TraversalLeetCode102.py
@@ -5,3 +5,26 @@
 # Input: root = [3,9,20,null,null,15,7]
 # Output: [[3],[9,20],[15,7]]
 # For BFS Breadth first search use Queue to implement the array
+import collections
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+
+    result = []
+
+    q = collections.deque()
+    q.append(root)
+
+    while q:
+        qlen = len(q)
+        level = []
+        for i in range(qlen):
+            node = q.popleft()
+            if node:
+                level.append(node.val)
+                q.append(node.left)
+                q.append(node.right)
+        result.append(level)
+
+        return result
